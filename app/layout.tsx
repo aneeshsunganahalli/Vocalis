d355b5312c45fd9cf05349be5cde1e096639db00
Aneesh <aneeshappid@gmail.com>
2025-04-28 01:25:12 +0530

    Auth Form Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.className} antialiased`}
+        className={`${geistSans.className} antialiased pattern`}
       >
         {children}
       </body>
