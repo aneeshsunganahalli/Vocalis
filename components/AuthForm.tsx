9e867a0b23ac3bf79dec3a520589a939933ef049
Aneesh <aneeshappid@gmail.com>
2025-04-29 00:57:30 +0530

    Auth Pages Completed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,22 +9,45 @@ import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import Image from "next/image"
 import Link from "next/link"
+import { toast } from "sonner"
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  })
+}
 
 const AuthForm = ({type} : {type: FormType}) => {
+  const router = useRouter()
+  const formSchema = authFormSchema(type)
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
   })
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === 'sign-up') {
+        toast.success("Account created successfully. PLease sign in.")
+        router.push('/sign-in')
+      } else {
+        toast.success("Signed in successfully.")
+        router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(`There was an error: ${error}`);
+    }
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
@@ -41,16 +64,35 @@ const AuthForm = ({type} : {type: FormType}) => {
         <h3>Practice job interviews with AI</h3>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-full form mt-4">
-            {!isSignIn && <p>Name</p>}
-            <p>Email</p>
-            <p>Password</p>
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Your Name"
+                />
+            )}
+            <FormField
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Your email address"
+                type="email"
+                />
+            <FormField
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Your Password"
+                type="password"
+                />
             <Button className="btn" type="submit">{isSignIn ? "Sign In" : "Create an Account"}</Button>
           </form>
         </Form>
 
         <p className="text-center">
           {isSignIn ? "Don't have an account?" : "Already have an account?"}
-          <Link href={isSignIn ? '/sign-in' : '/sign-up'} className='font-bold text-user-primary ml-1'>
+          <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className='font-bold text-user-primary ml-1'>
             {isSignIn ? "Sign Up" : "Sign In"}
           </Link>
         </p>
